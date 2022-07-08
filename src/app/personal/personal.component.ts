d8e03f66e1d640ebe6dc75ba0f5680460f7a81af
ajin5108 <ajinjayan6@gmail.com>
2022-07-07 23:35:08 -0700

    new changes

--- src/app/personal/personal.component.ts
+++ src/app/personal/personal.component.ts
@@ -8,8 +8,56 @@ import { Component, OnInit } from '@angular/core';
 export class PersonalComponent implements OnInit {
 
   constructor() { }
-
+  hello:Boolean=true
+  hello2:Boolean=false
+  hello3:boolean=false
+  hello4:boolean=false
+  hello5:boolean=false
+  hello6:boolean=false
+  hello7:boolean=false
   ngOnInit(): void {
   }
 
+  fnnext(){
+  this.hello=false
+  this.hello2=true
+  }
+  fnnext1(){
+    this.hello=false
+    this.hello2=false
+    this.hello3=true
+
+  }
+  fnnext2(){
+    this.hello=false
+    this.hello2=false
+    this.hello3=false
+    this.hello4=true
+  }
+  fnnext3(){
+    this.hello=false
+    this.hello2=false
+    this.hello3=false
+    this.hello4=false
+    this.hello5=true
+  }
+  fnnext4(){
+    this.hello=false
+    this.hello2=false
+    this.hello3=false
+    this.hello4=false
+    this.hello5=false
+    this.hello6=true
+
+  }
+  fnnext5(){
+    this.hello=false
+    this.hello2=false
+    this.hello3=false
+    this.hello4=false
+    this.hello5=false
+    this.hello6=false
+    this.hello7=true
+
+  }
 }
