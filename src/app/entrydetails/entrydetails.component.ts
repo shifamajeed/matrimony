8717ff8d267989611be03216c6c99364896c92da
ajin5108 <ajinjayan6@gmail.com>
2022-08-22 13:52:54 +0530

    ook

--- src/app/entrydetails/entrydetails.component.ts
+++ src/app/entrydetails/entrydetails.component.ts
@@ -85,6 +85,9 @@ export class EntrydetailsComponent implements OnInit {
     this.hello8=false
     this.hello9=true
   }
+  gotit(){
+    location.href="testside"
+  }
 //   function () {
 //     this.router.navigateByUrl('/editprofile');
 // }
