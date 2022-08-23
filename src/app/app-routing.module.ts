267bc245f3a08cb9e97800fd2970efe0f29853bf
shifamajeed <shifa4028@gmail.com>
2022-08-23 14:36:17 +0530

    finaaale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,28 +2,15 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { TestComponent } from './test/test.component';
-import { DeleteprofileComponent } from './deleteprofile/deleteprofile.component';
-import { HideprofileComponent } from './hideprofile/hideprofile.component';
 import { LeftsidenavComponent } from './leftsidenav/leftsidenav.component';
-import {SearchProfileComponent} from './search-profile/search-profile.component';
 import { EditprofileComponent } from './editprofile/editprofile.component';
-
-import { SubmitnsaveComponent } from './submitnsave/submitnsave.component'; 
-import { OpenwithmobileComponent } from './openwithmobile/openwithmobile.component';
-import { VerifyComponent } from './verify/verify.component';
-import { SuccessComponent } from './success/success.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 import {EntrydetailsComponent} from './entrydetails/entrydetails.component';
 import { NotificationComponent } from './notification/notification.component';
-import { Test2Component } from './test2/test2.component';
-
 import {TestsideComponent} from './testside/testside.component';
-
-
 import { Home2Component } from './home2/home2.component';
 
 
@@ -34,24 +21,13 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contactUs', component: ContactComponent },
   { path: 'test', component: TestComponent },
-  { path: 'delete', component: DeleteprofileComponent },
-  { path:'hide',component:HideprofileComponent },
   {path:'head',component:HeaderComponent},
   {path:'left',component:LeftsidenavComponent},
-  {path:'search',component:SearchProfileComponent},
   { path:'edit',component:EditprofileComponent},
-  { path: 'save', component: SubmitnsaveComponent },
-  { path: 'open', component: OpenwithmobileComponent},
-  { path: 'verify', component: VerifyComponent},
-  { path: 'success', component: SuccessComponent},
   { path: 'profile', component: ProfilesComponent},
   { path:'entry',component:EntrydetailsComponent},
   { path: 'noti', component: NotificationComponent},
-  { path:'test2', component: Test2Component},
-
   { path: 'testside',component: TestsideComponent},
-
-
   { path: 'hom', component: Home2Component},
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ModalComponent } from './modal/modal.component';
 import { TestComponent } from './test/test.component';
-import { DeleteprofileComponent } from './deleteprofile/deleteprofile.component';
-import { HideprofileComponent } from './hideprofile/hideprofile.component';
 import { LeftsidenavComponent } from './leftsidenav/leftsidenav.component';
-import { SearchProfileComponent } from './search-profile/search-profile.component';
 import { EditprofileComponent } from './editprofile/editprofile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
@@ -22,29 +19,19 @@ import { NotificationComponent } from './notification/notification.component';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
 import { EntrydetailsComponent } from './entrydetails/entrydetails.component'
 import { TestsideComponent } from './testside/testside.component';
 import { Home2Component } from './home2/home2.component';
 import { FormsModule } from '@angular/forms';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatStepperModule} from '@angular/material/stepper';
-
 import { ReactiveFormsModule } from '@angular/forms';
-import { Test2Component } from './test2/test2.component';
 
 
 
 
 
-// import { ModalModule } from './modal/modal.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { SubmitnsaveComponent } from './submitnsave/submitnsave.component';
-// import { Footer2Component } from './footer2/footer2.component';
-// import { OpenwithmobileComponent } from './openwithmobile/openwithmobile.component';
-// import { VerifyComponent } from './verify/verify.component';
-// import { SuccessComponent } from './success/success.component';
-// import { ProfilesComponent } from './profiles/profiles.component';
+
 
 
 
@@ -67,26 +54,12 @@ import { Test2Component } from './test2/test2.component';
     ContactComponent,
     ModalComponent,
     TestComponent,
-    DeleteprofileComponent,
-    HideprofileComponent,
     LeftsidenavComponent,
-    SearchProfileComponent,
     EditprofileComponent,
-
     EntrydetailsComponent,
-   
     NotificationComponent,
-
-        TestsideComponent,
-        Home2Component,
-
-
-        Test2Component,
-        
-
-    
-    
-    
+    TestsideComponent,
+    Home2Component,
 
   ],
   imports: [
